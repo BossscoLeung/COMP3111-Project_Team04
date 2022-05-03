3bf987c54bb86dec0dbfc22bc04e0ef7522ec101
Bosco Leung <boscokawa@gmail.com>
2022-05-03 13:06:08 +0800

    add comfeatureA test

--- src/main/java/comp3111/covid/ComFeatureA.java
+++ src/main/java/comp3111/covid/ComFeatureA.java
@@ -42,6 +42,14 @@ public class ComFeatureA {
 				}
 			}
 		}
+		System.out.print("no of data"+data.size()+"\n");
+		for (int i =0; i<data.size();i++) {
+			System.out.print(data.get(i).getXValue()+",");
+		}
+		System.out.print("\n");
+		for (int i =0; i<data.size();i++) {
+			System.out.print(data.get(i).getYValue()+",");
+		}
 		
 	}
 }
